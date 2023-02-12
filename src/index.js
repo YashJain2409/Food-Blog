e8eb3160e4e7725574a6f53bb927376cdc764e33
YashJain2409 <yashsj24@gmail.com>
2023-02-12 23:34:10 +0530

    change to default theme

--- src/index.js
+++ src/index.js
@@ -4,17 +4,6 @@ import "./index.css";
 import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
 
-import { extendTheme } from "@chakra-ui/react";
-
-const breakpoints = {
-  sm: "600px",
-  md: "768px",
-  lg: "960px",
-  xl: "1200px",
-  "2xl": "1536px",
-};
-
-const theme = extendTheme({ breakpoints });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
